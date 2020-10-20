227abd21bf6b8d6fb1b88d0b5535be872a414060
HunterCBishop <69934168+HunterCBishop@users.noreply.github.com>
2020-10-19 18:04:32 -0600

    Add files via upload

---
+++ Homework_1/Homework_9/sketch.js
@@ -0,0 +1,39 @@
+function setup()
+{
+createCanvas(500, 600);
+}
+
+function draw()
+{
+strokeWeight(6);
+background(300,2,17);
+fill(0, 204, 0);
+circle(250,120,120);
+   
+fill(0,204,0);
+rect(150,185,190,170)
+
+line(140,400,175,200);
+line(325,200,390,400);
+line(209,145,240,145);
+line(190,400,190,355);
+line(300,400,300,355);
+
+
+fill(0);
+point(215,120);
+point(235,120);
+
+fill(400,400,400);
+triangle(220,60,250,15,280,60)
+
+fill(120);
+textSize(32);
+text("HUNTER BISHOP",125,550 );
+fill(120);
+textSize(32);
+text("ITS ME!",300,40);
+
+
+
+}
